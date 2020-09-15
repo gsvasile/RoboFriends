768b212a25bcfa8a1ff993de381afdc6c1381a83
draghn <g.vaSile@gmail.com>
2020-09-15 00:48:53 -0700

    Initial Commit: Created custom Card, CardList react compononets

---
+++ src/SearchBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+    return (
+        <React.StrictMode>
+            <input
+                className="pa3 ba b--green bg-lightest-blue"
+                type='search'
+                placeholder='serach robots'
+                onChange={searchChange}
+            />
+            <br />
+        </React.StrictMode>
+    )
+}
+
+
+
+
+
+
+
+
+export default SearchBox;
